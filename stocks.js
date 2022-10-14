60bf6725c9e99c400f875e6449944aac92180287
Nikhil5705 <vermanikhil5705@gmail.com>
2022-10-14 14:15:18 +0530

    fixed 0 and negatve amount

--- stocks.js
+++ stocks.js
@@ -4,7 +4,7 @@ const currentPrice = document.querySelector("#current-price");
 const checkBtn = document.querySelector("#check-btn");
 const output = document.querySelector("#output");
 
-checkBtn.addEventListener("click", inputHandler);
+checkBtn.addEventListener("click", inputHandler)
 
 function inputHandler(){
 const ip = Number(initialPrice.value);
@@ -16,19 +16,36 @@ calculate(ip, sq, cp);
 }
 
 function calculate(initial, stocks, current){
-    if(current > initial){
+    
+    // if(initial==""||stocks==""||current=="")
+    // {   
+    //      output.innerText="Please fill all fields";
+    //      return 0;
+    // }
+     if(initial===0||stocks===0||current===0)
+    {   
+         output.innerText="Entered Value mut be greater than 0";
+         return 0;
+    }
+    else if(initial<0||stocks<0||current<0)
+    {   
+         output.innerText="Entered Value can not be Negative";
+         return 0;
+    }
+
+    else if(current > initial ){
         const profit = (current - initial)*stocks;
         const profitpercentage = (profit/initial)*100;
        output.innerText = "Profit is " + profit + " and Profit percentage is " +profitpercentage + "%";
        
      
      }
-         else if(current < initial){
+    else if(current < initial){
              const loss = (initial - current)*stocks;
              const losspercentage = (loss/initial)*100;
              output.innerText = "loss is " +loss + " and loss percentage is " +losspercentage + "%" ;
          }
-         else{
+    else{
              output.innerText = "No Loss No Gain, No Gain So Much Pain";
          }
 }
